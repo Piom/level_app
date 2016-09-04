2021c0a9beb29ca62814cdf7401442a7f4b7b394
Alexandr Korkin <av.korkin@gmail.com>
2016-09-04 12:12:36 +0300

    Initial commit

---
+++ level-app-web/src/main/java/ru/piom/mvc/level/GreetingController.java
@@ -0,0 +1,19 @@
+package ru.piom.mvc.level;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Alex on 02.09.2016.
+ */
+@Controller
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
