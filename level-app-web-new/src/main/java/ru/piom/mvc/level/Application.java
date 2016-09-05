ce98b8df0737e2ae366841bee9f1a5180cab81d8
Alexandr Korkin <av.korkin@gmail.com>
2016-09-05 19:59:47 +0300

    no message

---
+++ level-app-web-new/src/main/java/ru/piom/mvc/level/Application.java
@@ -0,0 +1,14 @@
+package ru.piom.mvc.level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Alex on 02.09.2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
